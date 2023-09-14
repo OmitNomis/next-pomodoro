d5240a27701c1c54859c344bfa00fd122cf10a62
OmitNomis <simon98190@gmail.com>
2023-09-14 16:32:30 +0545

    fix typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-purple-700 text-white py-4 text-center">
       <p className="text-lg sm:text-xl">
-        Made with <span className="text-red-500">&lt;3</span> by OmitNOmis
+        Made with <span className="text-red-500">&lt;3</span> by OmitNomis
       </p>
     </footer>
   );
